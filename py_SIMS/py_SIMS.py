2154f5576d52d05354750a6f5e19d0766073acd4
Stephen Zhou <zhoufwind@hotmail.com>
2014-07-01 03:17:35 -0700

    Update: Adding 'JHON' section

--- py_SIMS/py_SIMS.py
+++ py_SIMS/py_SIMS.py
@@ -1,1 +1,135 @@
-print('The Sims')
+#!/usr/bin/env python
+
+class Person():
+    def __init__(self, name, sex, age, lover, salary = 0):
+        self.name = name
+        self.sex = sex
+        self.age = age
+        self.lover = lover
+        self.salary = salary
+    def tell(self):
+        print "**********\n[John] My name is %s, I'm %s years old." % (self.name, self.age)
+    def learn(self, school):
+        self.school = school
+        print "[John] I learned course at %s." % self.school
+    def interview(self, company):
+        self.company = company
+        print "[John] So, I got an interview chance from: %s." % self.company
+    def change_work(self, company):
+        self.company = company
+        print "[John] I got a new job at %s." % self.company
+    def add_salary(self, salary):
+        self.salary = salary
+        print "[John] My salary is: %s now." % self.salary
+
+class School():
+    def __init__(self, name):
+        self.name = name
+
+class Company():
+    def __init__(self, name):
+        self.name = name
+
+def cont():
+    if raw_input('Continue...\n'):
+        pass
+
+def f_john():
+    name = 'John'
+    print "**********\n[System] Hi master, you are '%s' now, let me tell your new life..." % name.upper()
+    sex = 'Male'
+    age = 27
+    lover = 'Liz'
+    rival = 'Peter'
+    p_ds = Person(name, sex, age, lover)
+
+    p_ds.tell()
+
+    cont()
+    print "[John] %s is my lover, I meet her first time at high school, we love each other very much..." % lover
+
+    cont()
+    print "[John] After we graduated form high school, %s entered the 'Beijing City University' successfully, but I failed..." % lover
+
+    cont()
+    print "[John] I love %s very much. I went to Beijing as well, taking part time job at one web club, and paid her 4 year's tuition, just for living with her together." % lover
+
+    cont()
+    print "[John] However, the day came badlly for me. After %s graduated from the college and starting her career, she fell in love with his colleague -- %s, finnally, she left me without a word." % (lover, rival)
+
+    cont()
+    print "[John] I feel I'm going to die about it, my heart almost broken. How dare she did that to me? Is our is cheaper than money? I feel too tired to love any more..."
+
+    cont()
+    print "[John] After several days, I decide take %s back, I know all her want is money, I will hard work to earn much money which she can't spend them in all her life!" % lover
+
+    cont()
+    print "[John] For improving my ablity, I attend self-study exam, and go to school for studying."
+    while True:
+        s = raw_input('[System] Which school are you studying? ')
+        if len(s) == 0: continue
+        else:
+            break
+    school = School(s)
+    p_ds.learn(school.name)
+
+    cont()
+    print "[John] After several months time, I finished my course, and received many company's job introduce."
+    while True:
+        c = raw_input('[System] Which company you will interview? ')
+        if len(c) == 0: continue
+        else:
+            break
+    company = Company(c)
+    print "[John] Among them, I like %s best." % company.name
+    p_ds.interview(company.name)
+
+    cont()
+    print "[John] Easyly, I pass %s's interview, and her send the offer quickly." % company.name
+    p_ds.change_work(company.name)
+
+    cont()
+    print "[John] Many years later, I became rich, very rich, I am CTO at an Internation Internet company."
+    p_ds.salary = int(raw_input("[System] your salary now: "))
+    p_ds.add_salary(p_ds.salary)
+    print "[John] I can buy anything only if I want, but I can't buy %s which I loved." % lover
+
+    cont()
+    print "[John] One day, I meet %s, we have a short conversation, she told me, %s left her, she is alone now." % (lover, rival)
+
+    cont()
+    print "[John] I told her..."
+
+def main():
+    print '''
+***Welcome to SIMS***
+You can play following role:
+    J   : John
+    P   : Peter
+    L   : Liz
+***Enjob your new life***
+'''
+    
+    while True:
+        cmd = raw_input("Role: ").strip().lower()
+        if len(cmd) == 0: continue
+        elif cmd == 'j':
+            #print 'GO John'
+            f_john()
+        elif cmd == 'p':
+            print 'GO Peter'
+        elif cmd == 'l':
+            print 'GO Liz'
+        elif cmd == 'q':
+            print 'See you next time.'
+            break
+
+
+    #p_gfs = Person('Peter', 'm', 26)
+    #p_bfm = Person('Liz', 'f', 26)
+    #p_gfs.tell()
+    #p_bfm.tell()
+
+if __name__ == '__main__':
+    main()
+
